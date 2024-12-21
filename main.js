13e6e241eb8e0f48d85555842910c0ecadf9b331
marcusmoller97 <marcussmoller@icloud.com>
2024-12-21 16:05:53 +0100

    added createMovieCard into a function

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-import { createMovieCard } from "./movies.js";
+import { movieCard } from "./movies.js";
 
 //append one movie card to figure element with class movies (should maybe be changed later)
 // TODO: Read url, title from json file.
 // This needs to be done after issue #81
 const figureCard = document.querySelector('figure.movies');
-console.log(figureCard)
-createMovieCard("https://placecats.com/640/480", "Avatar", figureCard);
+movieCard.createMovieCard("https://placecats.com/640/480", "Avatar", figureCard);
+movieCard.createMovieCard("https://placecats.com/640/480", "Film2", figureCard);
