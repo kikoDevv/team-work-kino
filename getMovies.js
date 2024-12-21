12336bb766bdcd562bdd8428b21d425946387ad5
itsJoel59 <joel.j.jansson@gmail.com>
2024-12-21 11:32:32 +0100

    Adjusted functions in getMovies.js

--- getMovies.js
+++ getMovies.js
@@ -1,17 +1,53 @@
-async function getAllMovies() {
+//Function that adds all movies to an array
+async function getAllMovies(array) {
 
     fetch("./movies.json")
     .then(response => {
         return response.json();
     })
     .then(data => {
-        console.log(data);//Confirms that data has loaded correctly
-        
-        // **INSERT 
-        // ****CODE 
-        // *****FOR 
-        // ******CREATING 
-        // *******CARDS
+        data.forEach(element => {
+            array.push(element);
+        });
+        console.log(array);//Confirms that data has loaded correctly
+    })
+    .catch (error => {
+        console.error('Problem fetching:', error);
+    });
+}
+
+//Function that adds released movies to an array
+async function getReleasedMovies(array) {
+
+    fetch("./movies.json")
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        data.forEach(element => {
+            if (!element.comingSoon) //Checking if comingSoon is false
+            array.push(element);
+        });
+        console.log(array); //Confirms that data has loaded correctly
+    })
+    .catch (error => {
+        console.error('Problem fetching:', error);
+    });
+}
+
+//Function that adds upcoming movies to an array
+async function getUpcomingMovies(array) {
+
+    fetch("./movies.json")
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        data.forEach(element => {
+            if (element.comingSoon) //Checking if comingSoon is true
+            array.push(element);
+        });
+        console.log(array); //Confirms that data has loaded correctly
 
     })
     .catch (error => {
@@ -19,47 +55,13 @@ async function getAllMovies() {
     });
 }
 
-async function getReleasedMovies() {
-
-    fetch("./movies.json")
-    .then(response => {
-        return response.json();
-    })
-    .then(data => {
-        console.log(data);//Confirms that data has loaded correctly
-        
-        // **INSERT 
-        // ****CODE 
-        // *****FOR 
-        // ******CREATING 
-        // *******CARDS
-
-    })
-    .catch (error => {
-        console.error('Problem fetching:', error);
-    });
-}
-
-async function getUpcomingMovies() {
-
-    fetch("./movies.json")
-    .then(response => {
-        return response.json();
-    })
-    .then(data => {
-        console.log(data);//Confirms that data has loaded correctly
-        
-        // **INSERT 
-        // ****CODE 
-        // *****FOR 
-        // ******CREATING 
-        // *******CARDS
-
-    })
-    .catch (error => {
-        console.error('Problem fetching:', error);
-    });
-}
+//Example
+/* const movies = [], 
+released = [], 
+upcoming = [];
+getAllMovies(movies);
+getReleasedMovies(released);
+getUpcomingMovies(upcoming); */
 
 export {
     getAllMovies,
--- main.js
+++ main.js
@@ -1,5 +1,1 @@
 import { getAllMovies, getReleasedMovies, getUpcomingMovies } from './getMovies.js';
-
-getAllMovies();
-getReleasedMovies();
-getUpcomingMovies();
