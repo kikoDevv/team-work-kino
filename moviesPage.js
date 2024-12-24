b6108050e020ba312752962fb0b5c54209df1d43
Nasrolla <Nasrolla.hassani@gmail.com>
2024-12-24 02:50:50 +0100

    animation fixed

--- moviesPage.js
+++ moviesPage.js
@@ -2,14 +2,17 @@ document.addEventListener('DOMContentLoaded', () => {
     const searchSection = document.querySelector('.searchSection');
     const initialTop = searchSection.offsetTop;
     window.addEventListener('scroll', () => {
-        if (window.scrollY >= initialTop + 150) {
+        if (window.scrollY >= initialTop + 160) {
             searchSection.style.position = 'fixed';
-            searchSection.style.top = '50px';
-            searchSection.style.minWidth = "200px";
+            searchSection.style.top = '30px';
         } else {
             searchSection.style.position = 'absolute';
-            searchSection.style.top = '30%';
+            searchSection.style.top = '45%';
             searchSection.style.minWidth = "40vw";
         }
     });
+});
+
+window.addEventListener("scroll", ()=>{
+    console.log("value: " + window.scrollY);
 });
