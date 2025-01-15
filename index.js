ca24ffe87db15a6ba8c6b13c6ee469ad5e0dc39e
Christian Fryksten <christianfryksten1@gmail.com>
2025-01-15 16:58:10 +0100

    Updated index.ejs with the static information

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 const port = 5080;
 
+app.set("view engine", "ejs");
+//Berättar var alla ejs filer finns
+app.set("views", path.join(path.resolve(), "views"));
+
+// Servar statiska filer från public mappen
+app.use(express.static(path.join(path.resolve(), "public")));
+
+// Servar startsidan med dynamisk titel från EJS
 app.get("/", (req, res) => {
-  res.send("Tjenare");
+  res.render("index.ejs", { title: "Välkommen till kino!" });
 });
 
 // Startar servern
