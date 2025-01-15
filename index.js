e49817704ccfbcb993e174d92ebbc0f0226d50ca
Christian Fryksten <christianfryksten1@gmail.com>
2025-01-15 20:57:25 +0100

    Add logic when requesting info about movie

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.static(path.join(path.resolve(), "public")));
 //Middleware som loggar nice info :)
 app.use(morgan("tiny"));
 
-// fetchar från API
+// fetchar från API när request görs till startsidan
 app.get("/", async (req, res) => {
   try {
     const response = await fetch(
@@ -24,9 +24,25 @@ app.get("/", async (req, res) => {
     );
     const { data } = await response.json();
     res.render("index.ejs", { movies: data });
+    console.log(data);
   } catch (error) {
     console.error("Error fetching movies:", error);
     res.status(500).send("Could not fetch movies");
+  }
+});
+
+// Skapar en route när man gör request till /movies följd av ett id, ":" gör det dynamiskt och tillåter att ansöka med olika värden
+app.get("/movies/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const response = await fetch(
+      `https://plankton-app-xhkom.ondigitalocean.app/api/movies/${id}`
+    );
+    const { data } = await response.json();
+    res.send("Hello");
+    console.log(data);
+  } catch (error) {
+    console.error("Error fetching movie:", error);
   }
 });
 
